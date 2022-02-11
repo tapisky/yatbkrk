f6c658abae2aeac105bfeb1dd8893180a12ff1ac
tapisky <jose.carlos.tapiador.carretero@autodesk.com>
2022-02-11 15:39:43 +0100

    Fix typo

--- exchange.py
+++ exchange.py
@@ -6,7 +6,7 @@ from asynckraken import Client
 class Exchange:
     def __init__(self, api_key, api_secret, logger):
         self.krk_exchange = Client(key=api_key, secret=api_secret)
-        selg.logger = logger
+        self.logger = logger
 
     async def get_sell_price(self, pair):
         result = 0
